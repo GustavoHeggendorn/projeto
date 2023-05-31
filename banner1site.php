92c26106a458b8c51340003289b6c200aec40332
Fagundes762 <rafaelfagundes762@gmail.com>
2023-05-30 22:24:36 -0300

    layout dividido

---
+++ banner1site.php
@@ -0,0 +1,7 @@
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center section-bg">
+    <div class="container">
+      <div class="row justify-content-between gy-5">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7917.42720163554!2d-34.7981263!3d-7.1590726!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1685495914665!5m2!1spt-BR!2sbr" width="100%" height="1000" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </section><!-- End Hero Section -->
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+include_once('head.php');
+include_once('navbar.php'); 
+include_once('banner1site.php');
+?>
+  <main id="main">
+ <?php
+include_once('aboutus.php'); 
+include_once('divcontabilizadora.php');
+include_once('menu.php');
+include_once('comentarios.php');
+include_once('eventos.php');
+include_once('chef.php');
+include_once('registro.php');
+include_once('galeria.php'); 
+include_once('contato.php');
+?>
+</main><!-- End #main -->
+ <?php 
+include_once('footer.php');
+include_once('java.php');
+?> 
+ 
+
+  
+ 
+ 
+
+ 
